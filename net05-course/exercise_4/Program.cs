7d47efdbe41e090b6ff160233f5039b3c4cff919
HienNv59 <hiennv59@fpt.com>
2025-10-20 20:51:28 +0700

    feat(NET05): bài tập số 4

---
+++ net05-course/exercise_4/Program.cs
@@ -0,0 +1,39 @@
+﻿using exercise_4.Exercises;
+
+
+while (true)
+{
+    Console.WriteLine("***input (1)  --  lstNumber = [20, 81, 97, 63, 72, 11, 20, 15, 33, 15, 41, 20]");
+    Console.WriteLine("***input (2)  --  lstStrings = [apple, banana, orange, kiwi, mango, pineapple, grape, melon]");
+    Console.WriteLine("*** 0 : exit");
+
+    Console.Write("select: ");
+
+    string input = Console.ReadLine();
+
+    bool isInteger = int.TryParse(input, out int selected);
+
+    if (!isInteger)
+    {
+        Console.WriteLine("retry!");
+        continue;
+    }
+    Console.WriteLine("");
+
+    switch (selected)
+    {
+        case 0:
+            Console.WriteLine("exit");
+            return;
+        case 1:
+            Exercise1.Run();
+            break;
+        case 2:
+            // Exercise02.Run();
+            break;
+        default:
+            Console.WriteLine("retry!");
+            break;
+    }
+}
+
